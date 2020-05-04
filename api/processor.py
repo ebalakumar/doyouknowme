513a009fcffb4f024eb5bd740e413251f8503ef9
Balakumar Ezhilmaran <ebalakumar@yahoo.co.in>
2020-05-04 16:15:58 +0530

    sample flask-restful program

--- api/processor.py
+++ api/processor.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 
-app = Flask(__name__)
-api = Api(app)
+application = Flask(__name__)
+api = Api(application)
 
 TODOS = {
     'todo1': {'task': 'build an API'},
@@ -58,4 +58,4 @@ api.add_resource(TodoList, '/todos')
 api.add_resource(Todo, '/todos/<todo_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True, host='0.0.0.0', port=5000)
