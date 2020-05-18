cbdea348cdc6342f65ed3dda04306df5708bbce0
Balakumar Ezhilmaran <ebalakumar@yahoo.co.in>
2020-05-18 23:01:59 +0530

    Verify: Refactored

--- lambda_upload_s3.py
+++ lambda_upload_s3.py
@@ -1,5 +1,3 @@
-import json
-import urllib.parse
 import time
 import boto3
 import base64
@@ -8,12 +6,6 @@ import os
 
 
 def lambda_handler(event, context):
-    # raise Exception('Something went wrong')
-    # print("Received event: " + json.dumps(event, indent=2))
-
-    # Get the object from the event and show its content type
-    # bucket = event['Records'][0]['s3']['bucket']['name']
-    # key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
         if event['operation'] == "upload":
             if verify_secret_key(event["secret_code"]):
@@ -22,23 +14,9 @@ def lambda_handler(event, context):
                 return "invalid secret code"  # todo: return proper error type
         elif event['operation'] == "verify":
             response = image_verification(event)
-
         return response
-
-        # response = s3.get_object(Bucket=bucket, Key=key)
-        # print("CONTENT TYPE: " + response['ContentType'])
-        # return response['ContentType']
-        # print("Received event: " + json.dumps(event, indent=2))
-        # print("value1 = " + event['key1'])
-        # print("value2 = " + event['key2'])
-        # print("value3 = " + event['key3'])
-        # return event['key1']  # Echo back the first key value
     except Exception as e:
         print(e)
-        # print(
-        #     'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as '
-        #     'this function.'.format(
-        #         key, bucket))
         raise e
 
 
@@ -91,7 +69,7 @@ def add_update_user_details(event):
 def image_verification(event):
     client = boto3.client('rekognition')
     existing_images = get_images(event["user_id"])  # todo: condition if get_image is empty
-    print("EXISTING IMAGES OBJ", existing_images)
+    # print("EXISTING IMAGES OBJ", existing_images)
     responses = []
     for img in existing_images:
         response = client.compare_faces(
@@ -103,23 +81,19 @@ def image_verification(event):
             },
             SimilarityThreshold=90
         )["FaceMatches"]
-
-        print("breakpoint", response)
+        # print("breakpoint", response)
         if not response:
             message = "This Image is not part of your existing collection"
         else:
-            message = "Face detected in image and its " + str(response[0]['Similarity']) + " match"
-
+            message = "Face detected in image and its a " + str(round(int(response[0]['Similarity']), 2)) + "% match"
         responses.append({
             "image_data": img['ImageData']['S'],
             "message": message
         })
-
     return responses
 
 
 def get_images(user_id):
-    # client = boto3.client('dynamodb')
     table = os.environ['DYNAMODB_TABLE']
     client = boto3.client('dynamodb', endpoint_url='http://localhost:32768')
     response = client.query(
