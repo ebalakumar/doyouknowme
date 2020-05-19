60711d78e65620d84254e48533fb564c77a13454
Bala <40795470+ebalakumar@users.noreply.github.com>
2020-05-19 16:30:44 +0530

    Added region variable

--- infra/tf_setup.tf
+++ infra/tf_setup.tf
@@ -6,8 +6,13 @@ variable "secret_key" {
   type = string
 }
 
+variable "region" {
+  type = string
+}
+
 provider "aws" {
   version = "~> 2.0"
   access_key = var.access_key
   secret_key = var.secret_key
+  region = var.region
 }
