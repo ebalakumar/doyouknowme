35c201ce9ed854a8f29765d18dd6f8f69aace625
Balakumar Ezhilmaran <ebalakumar@yahoo.co.in>
2020-05-19 17:00:48 +0530

    Attaching iam policies for accessing ImageDetails table and aws rekon

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,68 @@
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda_test"
+resource "aws_iam_policy" "processor_access_ImageDetails" {
+  name = "processor_access_ImageDetails"
+  path = "/"
+  description = "Allowing Processor to access ImageDetails table"
 
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:BatchGetItem",
+                "dynamodb:PutItem",
+                "dynamodb:DescribeTable",
+                "dynamodb:GetItem",
+                "dynamodb:Scan",
+                "dynamodb:Query",
+                "dynamodb:UpdateItem"
+            ],
+            "Resource": [
+                "arn:aws:dynamodb:ap-south-1:818087033500:table/ImageDetails",
+                "arn:aws:dynamodb:ap-south-1:818087033500:table/ImageDetails/index/UserId-index"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:ListContributorInsights",
+                "dynamodb:DescribeReservedCapacityOfferings",
+                "dynamodb:ListGlobalTables",
+                "dynamodb:ListTables",
+                "dynamodb:DescribeReservedCapacity",
+                "dynamodb:ListBackups",
+                "dynamodb:DescribeLimits",
+                "dynamodb:ListStreams"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "processor_use_aws_rekon" {
+  name = "processor_use_aws_rekon"
+  path = "/"
+  description = "Allowing Processor to use AWS Rekognition"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "rekognition:CompareFaces",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "processor_role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -16,6 +78,16 @@ resource "aws_iam_role" "iam_for_lambda" {
   ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy_attachment" "processor_imagedetails_role_attachment" {
+  role = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.processor_access_ImageDetails.arn
+}
+
+resource "aws_iam_role_policy_attachment" "processor_aws_rekon_role_attachment" {
+  role = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.processor_use_aws_rekon.arn
 }
 
 //resource "aws_lambda_function" "processor_lambda" {
