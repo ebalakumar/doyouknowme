e53c81bf7b8b7b447dd964f266bd7cd33a866d78
Balakumar Ezhilmaran <ebalakumar@yahoo.co.in>
2020-05-20 18:57:42 +0530

    Updated GSI projection_type TO ALL

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -22,7 +22,7 @@ resource "aws_dynamodb_table" "image_details" {
     //    range_key          = "TopScore"
     write_capacity = 5
     read_capacity = 5
-    projection_type = "KEYS_ONLY"
+    projection_type = "ALL"
   }
 
   //  tags = {
